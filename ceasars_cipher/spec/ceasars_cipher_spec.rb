01ba0a615ef9edc9235bc379d34cb55f80796285
Arthur Baker <artbaker82@gmail.com>
2022-01-16 12:41:27 -0800

    cleaned up code

--- ceasars_cipher/spec/ceasars_cipher_spec.rb
+++ ceasars_cipher/spec/ceasars_cipher_spec.rb
@@ -5,4 +5,15 @@ describe 'ceasars_cipher exercise' do
   it 'shifts by 5' do
     expect(ceasar_cipher("What a string!", 5)).to eq("Bmfy f xywnsl!")
   end
+
+  it 'shifts by 13' do
+    expect(ceasar_cipher("Hello Emmaline!", 13)).to eq("Uryyb Rzznyvar!")
+  end
+
+  it 'shifts by 26' do
+    expect(ceasar_cipher("Hello Emmaline!", 26)).to eq("Hello Emmaline!")
+  end
+
+
+  
 end
