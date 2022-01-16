758ac5d7deb783c7a2149b99335be1cfab547d2d
Arthur Baker <artbaker82@gmail.com>
2022-01-16 15:32:43 -0800

    faster stock picker implementation

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -26,4 +26,26 @@ end
 
 #this works/ but nested loops make it slow for large inputs (leetcode test case)
 
-# make it faster using one loop and two pointers
+# make it faster using one loop 
+
+def stock_picker_2(prices)
+  length = prices.length
+  highest_profit = 0
+  min_buy = prices[0]
+
+  (1..prices.length - 1).each do |i|
+
+    if prices[i] < min_buy
+      min_buy = prices[i]
+    end
+
+    if prices[i] - min_buy > highest_profit 
+      highest_profit = prices[i] - min_buy
+    end
+    
+  end
+
+  highest_profit
+end
+
+stock_picker_2([7,1,5,3,6,4])
